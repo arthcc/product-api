661bdfa958e5521ecd1a8aad3fe38128cbc1b41e
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-03-31 14:26:09 -0300

    first commit

---
+++ spring_api/src/main/java/Service/ProductService.java
@@ -0,0 +1,16 @@
+package Service;
+
+import Model.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+    List<Product> findAll();
+    Optional<Product> findById(Long id);
+    Product save(Product product);
+
+    Product update(Product product);
+
+    void deleteById(Long id);
+}
